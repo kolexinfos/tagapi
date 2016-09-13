f2f0e77b0e1c30d79d3426178182dd97ebac954b
Kehinde Kolade <kolexinfos@gmail.com>
2016-09-13 06:10:24 +0100

    Update Db Information to settings from mLab

--- config/main.js
+++ config/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'longobnoxiouspassphrase',
-  'database': 'mongodb://localhost:saas-tutorial/saas-tutorial'
+  'database': 'mongodb://tag:tagon@ds044699.mlab.com:44699/tag'
 };
